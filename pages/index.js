f3e12554893009404e3e71cc3e39a259876712f3
Akash Kandasamy <akashram953@gmail.com>
2022-01-19 22:05:26 +0530

    revision 1

--- pages/index.js
+++ pages/index.js
@@ -11,21 +11,21 @@ export default function Home() {
       <div className={styles.sect}>
         <div className={styles.container}>
           <div className={styles.text}>
-            <p className={styles.p1}>A Platform to visualise your life&apos;s memory lanes</p>
-            <p className={styles.p2}>Capture memories in even more inclusive, immersive & innovative style.</p>
+            <p className={styles.p1}>A Platform to visualise your <br/>life&apos;s memory lanes</p>
+            <p className={styles.p2}>Capture memories in even more<br/> inclusive, immersive & innovative style.</p>
             <div className={styles.b_cont}>
               <button className={styles.button1}>Contact Us!</button>
               <button className={styles.button2}>Sign Up</button>
             </div>
             <div className={styles.c_cont}>
               <p className={styles.p_text}>Comming Soon!</p>
-              <div>
-                <Image className={styles.app_store} src='/app_store.svg' layout="fixed" height={100} width={100} alt='app store'/>
-                <Image className={styles.play_store}src='/play_store.svg' layout="fixed" height={100} width={100} alt='play store'/>
+              <div className={styles.img_cont}>
+                <div className={styles.app_store}><Image  src='/app_store.svg' layout="fixed" height={55} width={165} alt='app store'/></div>
+                <div className={styles.play_store}><Image src='/play_store.svg' layout="fixed" height={55} width={165} alt='play store'/></div>
               </div>
             </div>
           </div>
-          <Image src='/home.svg' width={700} height={500} layout='fixed' className={styles.image} alt='home image'/>
+          <Image src='/home.svg' width={824.5} height={490.57} layout='fixed' className={styles.image} alt='home image'/>
         </div>
         
       </div>
