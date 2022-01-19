f9d0e778251b0b3594ef14e2b21643b80ff0836c
Akash Kandasamy <akashram953@gmail.com>
2022-01-19 12:34:17 +0530

    home body

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,29 @@ import bg from '../res/bg.svg'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.cont}>
       <Navbar/>
-      <div className={styles.cont}></div>
+      <div className={styles.sect}>
+        <div className={styles.container}>
+          <div className={styles.text}>
+            <p className={styles.p1}>A Platform to visualise your life's memory lanes</p>
+            <p className={styles.p2}>Capture memories in even more inclusive, immersive & innovative style.</p>
+            <div className={styles.b_cont}>
+              <button className={styles.button1}>Contact Us!</button>
+              <button className={styles.button2}>Sign Up</button>
+            </div>
+            <div className={styles.c_cont}>
+              <p className={styles.p_text}>Comming Soon!</p>
+              <div>
+                <Image className={styles.app_store} src='/app_store.svg' layout="fixed" height={100} width={100}/>
+                <Image src='/play_store.svg' layout="fixed" height={100} width={100}/>
+              </div>
+            </div>
+          </div>
+          <Image src='/home.svg' width={700} height={500} layout='fixed' className={styles.image}/>
+        </div>
+        
+      </div>
     </div>
 
   )
